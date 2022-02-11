c70ccfac75e8d1c0d669997ff3d9968e2998cb80
小林大輝 <kobayashidaiki@daikiPC.local>
2022-02-11 09:42:34 +0900

    [Finish]1-6

--- app.js
+++ app.js
@@ -68,12 +68,41 @@
 //   });
 // });
 
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color': '#0000ff',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp();
+//   });
+// });
+
+
+// 1-6
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000ff'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#ff0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': '#0000ff',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
