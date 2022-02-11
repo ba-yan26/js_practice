81a6828c732f7eb44c5244c5c8cc19b2b4757f95
小林大輝 <kobayashidaiki@daikiPC.local>
2022-02-11 10:02:30 +0900

    [Finish]1-7

--- app.js
+++ app.js
@@ -98,11 +98,25 @@
 //   });
 // });
 
+// $(function(){
+//   $('.box1').on('click',function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+
+// 1-7
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
 $(function(){
-  $('.box1').on('click',function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
   });
 });
